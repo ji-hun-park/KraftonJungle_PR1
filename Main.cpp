3e7269067fa607b48605cc353a8b66322e35ffdd
JiHun <pgh979@gmail.com>
2026-01-20 10:30:56 +0900

    캡슐화 테스트

--- Main.cpp
+++ Main.cpp
@@ -1,29 +1,25 @@
 #include <iostream>
 using namespace std;
 
-// Create a Car class with some attributes
-class Car {
+class Employee {
+private:
+    // Private attribute
+    int salary;
+
 public:
-    string brand;
-    string model;
-    int year;
+    // Setter
+    void setSalary(int s) {
+        salary = s;
+    }
+    // Getter
+    int getSalary() {
+        return salary;
+    }
 };
 
 int main() {
-    // Create an object of Car
-    Car carObj1;
-    carObj1.brand = "BMW";
-    carObj1.model = "X5";
-    carObj1.year = 1999;
-
-    // Create another object of Car
-    Car carObj2;
-    carObj2.brand = "Ford";
-    carObj2.model = "Mustang";
-    carObj2.year = 1969;
-
-    // Print attribute values
-    cout << carObj1.brand << " " << carObj1.model << " " << carObj1.year << "\n";
-    cout << carObj2.brand << " " << carObj2.model << " " << carObj2.year << "\n";
+    Employee myObj;
+    myObj.setSalary(50000);
+    cout << myObj.getSalary();
     return 0;
 }
