6b71d284fddaf9b186e101eec6e368eb86873655
JiHun <pgh979@gmail.com>
2026-01-20 09:59:26 +0900

    조건문 테스트

--- Main.cpp
+++ Main.cpp
@@ -2,37 +2,16 @@
 using namespace std;
 
 int main() {
-	struct car
-	{
-		int year;
-		string brand;
-		string model;
-	};
-
-	car myCar1;
-	car myCar2;
-	car myCar3;
-
-	int i;
-	string s1, s2;
-
-	myCar1.year = 1999;
-	myCar1.brand = "BMW";
-	myCar1.model = "X5";
-
-	myCar2.year = 1969;
-	myCar2.brand = "Ford";
-	myCar2.model = "Mustang";
-
-	cin >> i;
-	cin >> s1;
-	cin >> s2;
-	myCar3.year = i;
-	myCar3.brand = s1;
-	myCar3.model = s2;
-
-	cout << myCar3.brand << " " << myCar3.model << " " << myCar3.year << endl;
-	//cout << myCar2.brand << " " << myCar2.model << " " << myCar2.year << endl;
-
+	int x = 20;
+	int y = 20;
+	if (x > y) {
+		cout << "x is greater than y" << endl;
+	}
+	else if (x == y) {
+		cout << "x is equal to y" << endl;
+	}
+	else {
+		cout << "x is not greater than y" << endl;
+	}
 	return 0;
 }
